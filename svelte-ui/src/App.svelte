e3a85cd3b121562d14da48e70e3223013e0ee9a4
Paul Yang <paul.junho.yang@gmail.com>
2020-11-01 19:49:20 -0800

    init repo

---
+++ svelte-ui/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import ClockButton from './ClockButton.svelte';
+	let user = 'Paul';
+</script>
+
+<main> 
+	<h3>Hi {user}</h3>
+	<ClockButton />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h3 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-ui/src/ClockButton.svelte
@@ -0,0 +1,21 @@
+<script>
+    import dayjs from 'dayjs';
+    
+    currTime = dayjs();
+    console.log(currTime)
+
+    // const minuteInMs = 60000;
+    function timeInHoursAndMinutes() {
+        return currTime.format('HH:mm')
+    }
+</script>
+
+<style>
+    button {
+
+    }
+</style>
+
+<button> HEllo
+    <!-- {timeInHoursAndMinutes()} -->
+</button>
